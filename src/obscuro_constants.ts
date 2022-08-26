e9e756e0446f3beb4bb735f30676bf18613d9fcb
Pedro Gomes <otherview@gmail.com>
2022-08-26 12:06:14 +0100

    updating constant names

--- src/obscuro_constants.ts
+++ src/obscuro_constants.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-export const state = {"v3CoreFactoryAddress":"0xFeD8Fc00f96d652244c6EE628da65Ea766CcEc81","multicall2Address":"0xf8ABf479901e2a59Ab7A371E250F592547C8bf53","proxyAdminAddress":"0x1e553a8b477FaBaA664e2372FBc2C6Ce52A14E74","tickLensAddress":"0xD32F131F6f070f544b917b13f32715A12BE4b98A","nftDescriptorLibraryAddressV1_3_0":"0x0eb842561eCa996eBbf874e0D7bd4636854381C3","nonfungibleTokenPositionDescriptorAddressV1_3_0":"0xe536c858f25EF284991d1a28A1B899E521871329","descriptorProxyAddress":"0x862b832FD75f1a8904f82Bfd69F3f987E3c2c095","nonfungibleTokenPositionManagerAddress":"0x1999B7Eb94dF3dF5b634a5715b082fA8aadE142b","v3MigratorAddress":"0xE678e46Dddc60229143AFE74c5188c26acD31C0D","v3StakerAddress":"0xF43DaC352E2f11C1C9a0f03445B492492264bCa6","quoterV2Address":"0x2812a66FD5996Cef0C5b1b9688CB4A4578ff5DA6","swapRouter02":"0xF475CC602e0d4a7776701efB0837765185389d9D"}
-export const erc20state = {"wethAddress":"0x890e32E4b52915819E36A3A085Bd466b3e518d18", "usdcAddress": "0x3A8F3d0Eb8dA18da9e7Eb0DE1BC35d62B87c2eD4"}
+export const erc20state ={"WETHAddress":"0xe161409451acC89081C9BA75B00119eE337a0A07","PTKAddress":"0xf6aB0a8B47801ee51f3114197d2C764e1e5a3528","OBXAddress":"0xb99405F3020Ad1A3a8c3F24A003800b52D17F0eE","USDCAddress":"0x0053E8dE4199AE8Ee8CA8e55A919817F822a05F1"}
+export const state = {"v3CoreFactoryAddress":"0x909b6C714Cc86FFaBAA9AC8ef2582ec2b904d9F8","multicall2Address":"0xCf9D73d8524921D1290091F68992FdD1e0fC32C6","proxyAdminAddress":"0x7e8648990841145091fb27BAFfF8798db9fd6346","tickLensAddress":"0xc0DF484b68E454c37b59F6282F85305Bde9DaB64","nftDescriptorLibraryAddressV1_3_0":"0x3aA920b69D701ec42Fdf8E38c91647a1bf92e5dF","nonfungibleTokenPositionDescriptorAddressV1_3_0":"0xC6012A846C234A3d398946E58354f00Aa546e184","descriptorProxyAddress":"0x6336DCf5C785EDd7239E9E6C115F5d8AE9a2dd08","nonfungibleTokenPositionManagerAddress":"0x6bF3FA45c6Ef906F8E25462FCE6a529723DaA80E","v3MigratorAddress":"0x6d3f34B6c73c026C25105B148907aD882b34f840","v3StakerAddress":"0x4569fB13116d2DF2A27F429859F84B87CbC20d2B","quoterV2Address":"0xA037c4d6455229C88c80Dcad2dDA65244462c4f9","swapRouter02":"0x35368B8F6ac1746faed726F6E930235568457f9D"}
 // Do not remove lines above
 
 export const OBSCURO_NETWORK_V3_CORE_FACTORY_ADDRESSES = state.v3CoreFactoryAddress
@@ -12,8 +12,8 @@ export const OBSCURO_NETWORK_QUOTER_ADDRESSES = state.quoterV2Address
 export const OBSCURO_NETWORK_NONFUNGIBLE_POSITION_MANAGER_ADDRESSES = state.nonfungibleTokenPositionManagerAddress
 export const OBSCURO_NETWORK_TICK_LENS_ADDRESSES = state.tickLensAddress
 
-export const OBSCURO_NETWORK_WETH_ADDRESS = erc20state.wethAddress
-export const OBSCURO_NETWORK_USDC_ADDRESS = erc20state.usdcAddress
+export const OBSCURO_NETWORK_WETH_ADDRESS = erc20state.WETHAddress
+export const OBSCURO_NETWORK_USDC_ADDRESS = erc20state.USDCAddress
 
 export const OBSCURO_NETWORK_URL = 'http://127.0.0.1:3001'
 
